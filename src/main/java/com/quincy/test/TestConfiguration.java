0ee17eb4b6850c57b7ed7765fc13b9c659c8cf50
Ma Qiang <mq7253180@126.com>
2025-12-30 16:55:54 +0800

    xxx

---
+++ src/main/java/com/quincy/test/TestConfiguration.java
@@ -0,0 +1,46 @@
+package com.quincy.test;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Configuration
+public class TestConfiguration implements WebMvcConfigurer, HandlerInterceptor {
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(this);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("*");
+//			.allowedOriginPatterns("http://jep8566.com", "http://*.jep8566.com", "http://www.xxx.com", "http://localhost:*")
+//			.allowCredentials(true)
+//			.allowedHeaders("*")
+//			.allowedMethods("*");
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+//		response.setHeader("Access-Control-Allow-Origin", "http://jep8566.com");
+//		response.setHeader("Access-Control-Allow-Credentials", "true");
+//		response.setHeader("Access-Control-Allow-Headers", "Authorization, x-requested-with, content-type, x-token");
+//		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+				@Nullable ModelAndView modelAndView) throws Exception {
+		
+	}
+}
--- src/main/java/com/quincy/test/TestController.java
+++ src/main/java/com/quincy/test/TestController.java
@@ -1,14 +1,13 @@
 package com.quincy.test;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
-	@GetMapping("/abc")
+	@RequestMapping("/abc")
 	@ResponseBody
 	public Result abc() {
 		Result r = new Result();
